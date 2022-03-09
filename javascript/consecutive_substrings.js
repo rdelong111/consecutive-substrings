cdbae1c1e961e8824dd7ac52086bafe092467302
RyanDeLong <rdelong111@gmail.com>
2022-03-09 14:29:58 -0600

    commit tree 42f35a89da12e7f8953c57ac2710f26c9d1fcd54

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,5 +1,14 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const ans = [];
+
+  for (let i = 0; i < string.length; i++) {
+    ans.push(string[i]);
+    for (let j = i + 1; j < string.length; j++) {
+      ans.push(string.slice(i, j + 1));
+    }
+  }
+
+  return ans;
 }
 
 if (require.main === module) {
@@ -15,5 +24,20 @@ if (require.main === module) {
 
 module.exports = consecutiveSubstrings;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+Initialize an empty array called ans
+
+Loop length of string times labeling each iteration i
+  Push the i'th element to ans
+  Loop length string times labeling each iteration j and starting at i
+    Push the substring (i, j) of string to ans
+
+Return ans
+*/
+
+/*
+An empty array is initialized to store the strings.
+A FOR loop is created to loop through the elements of string. Each iteration of this loop will push a single letter to ans.
+A nested FOR loop is created to loop through the elements of string starting at the element after the current iteration of the parent FOR loop. The substring (i, j) is pushed to ans.
+Ans is returned.
+*/
